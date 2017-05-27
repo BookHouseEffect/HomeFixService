768fed1f97a664b8bb4e8df1815c17d8950e7b8b
Ѓорче Цековски <cekovski.gjorche@students.finki.ukim.mk>
2017-05-27 21:53:39 +0200

    Creating (Initial) migrations script

---
+++ HomeFixService.WebService/Migrations/201705271934596_InitialCreate.cs
@@ -0,0 +1,174 @@
+namespace HomeFixService.WebService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BusySchedules",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        BusyPeriodStartOn = c.DateTime(nullable: false),
+                        BusyPeriodEndsOn = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Id, t.UserId })
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserFirstName = c.String(nullable: false),
+                        UserLastName = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.UserAddresses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        StreetName = c.String(nullable: false),
+                        City = c.String(nullable: false),
+                        Country = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Id, t.UserId })
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.UserProfessions",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false),
+                        TheProfession = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.Users", t => t.UserId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Ratings",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        FeedbackDateTime = c.DateTime(nullable: false),
+                        FeedbackPoints = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Id, t.UserId })
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Services",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        ServiceName = c.String(nullable: false),
+                        ServiceUnit = c.String(nullable: false),
+                        ServiceUnitPrice = c.Single(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Id, t.UserId })
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        PhoneNumber = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Id, t.UserId })
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.TimeSchedules",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        WorkingDayOfWeek = c.Int(nullable: false),
+                        StartTime = c.Time(nullable: false, precision: 7),
+                        EndTime = c.Time(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => new { t.Id, t.UserId })
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Credentials",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        UserName = c.String(nullable: false),
+                        PasswordHash = c.String(nullable: false),
+                        PasswordSalt = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Id, t.UserId })
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.UserPasswordsHistories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CredentialsId = c.Int(nullable: false),
+                        UserId = c.Int(nullable: false),
+                        PasswordHash = c.String(nullable: false),
+                        PasswordSalt = c.String(nullable: false),
+                        ExpiredOn = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Id, t.CredentialsId, t.UserId })
+                .ForeignKey("dbo.Credentials", t => new { t.CredentialsId, t.UserId }, cascadeDelete: true)
+                .Index(t => new { t.CredentialsId, t.UserId });
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Credentials", "UserId", "dbo.Users");
+            DropForeignKey("dbo.UserPasswordsHistories", new[] { "CredentialsId", "UserId" }, "dbo.Credentials");
+            DropForeignKey("dbo.TimeSchedules", "UserId", "dbo.Users");
+            DropForeignKey("dbo.Contacts", "UserId", "dbo.Users");
+            DropForeignKey("dbo.Services", "UserId", "dbo.Users");
+            DropForeignKey("dbo.Ratings", "UserId", "dbo.Users");
+            DropForeignKey("dbo.UserProfessions", "UserId", "dbo.Users");
+            DropForeignKey("dbo.BusySchedules", "UserId", "dbo.Users");
+            DropForeignKey("dbo.UserAddresses", "UserId", "dbo.Users");
+            DropIndex("dbo.UserPasswordsHistories", new[] { "CredentialsId", "UserId" });
+            DropIndex("dbo.Credentials", new[] { "UserId" });
+            DropIndex("dbo.TimeSchedules", new[] { "UserId" });
+            DropIndex("dbo.Contacts", new[] { "UserId" });
+            DropIndex("dbo.Services", new[] { "UserId" });
+            DropIndex("dbo.Ratings", new[] { "UserId" });
+            DropIndex("dbo.UserProfessions", new[] { "UserId" });
+            DropIndex("dbo.UserAddresses", new[] { "UserId" });
+            DropIndex("dbo.BusySchedules", new[] { "UserId" });
+            DropTable("dbo.UserPasswordsHistories");
+            DropTable("dbo.Credentials");
+            DropTable("dbo.TimeSchedules");
+            DropTable("dbo.Contacts");
+            DropTable("dbo.Services");
+            DropTable("dbo.Ratings");
+            DropTable("dbo.UserProfessions");
+            DropTable("dbo.UserAddresses");
+            DropTable("dbo.Users");
+            DropTable("dbo.BusySchedules");
+        }
+    }
+}
---
+++ HomeFixService.WebService/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace HomeFixService.WebService.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HomeFixService.WebService.Models.Context.DatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(HomeFixService.WebService.Models.Context.DatabaseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
