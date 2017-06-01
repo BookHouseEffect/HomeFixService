ae5bf5024447a9383fca106f9d4ccfe41c0541af
Ѓорче Цековски <cekovski.gjorche@students.finki.ukim.mk>
2017-06-01 18:51:03 +0200

    Exceptions moved under models.

--- HomeFixService.WebService/Models/Exeptions/NewPasswordCannotBeAsOneOfOldPasswordsException.cs
+++ HomeFixService.WebService/Models/Exeptions/NewPasswordCannotBeAsOneOfOldPasswordsException.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace HomeFixService.WebService.Services.Exeptions
+namespace HomeFixService.WebService.Models.Exeptions
 {
     public class NewPasswordCannotBeAsOneOfOldPasswordsException : Exception
     {
--- HomeFixService.WebService/Models/Exeptions/NoEntryFoundException.cs
+++ HomeFixService.WebService/Models/Exeptions/NoEntryFoundException.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace HomeFixService.WebService.Services.Exeptions
+namespace HomeFixService.WebService.Models.Exeptions
 {
     public class NoEntryFoundException : Exception
     {
+        public int Id { get; }
         public int UserId { get; }
         public string ExceptionInTable { get; }
 
@@ -18,7 +19,21 @@ namespace HomeFixService.WebService.Services.Exeptions
                      exceptionInTable)
                  )
         {
-            this.UserId = id;
+            this.Id = id;
+            this.ExceptionInTable = exceptionInTable;
+        }
+
+        public NoEntryFoundException(int id, int userId, string exceptionInTable)
+            : base(
+                 String.Format(
+                     "No entry with the key {0} for user with id {1} found from {2} table.",
+                     id,
+                     userId,
+                     exceptionInTable)
+                 )
+        {
+            this.Id = id;
+            this.UserId = userId;
             this.ExceptionInTable = exceptionInTable;
         }
 
