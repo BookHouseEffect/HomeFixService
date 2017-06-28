3e4bbd9008d4c489d8e849be9fe86c46d5f1451d
mihailnacev <m.nacev@yahoo.com>
2017-06-28 11:20:22 +0200

    Update ProfessionsEnum.cs

--- HomeFixService.WebService/Models/Enums/ProfessionsEnum.cs
+++ HomeFixService.WebService/Models/Enums/ProfessionsEnum.cs
@@ -5,6 +5,14 @@ namespace HomeFixService.WebService.Models.Enums
     public enum ProfessionsEnum 
     {
         [Description("Cleaning service")] HYGIEN = 1,
-        [Description("Power service")] ELECTRITIAN
+        [Description("Power service")] ELECTRITIAN,
+        [Description("Bathroom service")] PLUMBER,
+        [Description("House wall service")] PAINTER,
+        [Description("Child care service")] BABYSITTER,
+        [Description("Shoe fixing service")] SHOEMAKER,
+        [Description("Transport service")] TAXIDRIVER,
+        [Description("Clothes cutting service")] TAILOR,
+        [Description("IT service")] PROGRAMMER,
+        [Description("Defense/Protecting service")] BODYGUARD
     }
 }
